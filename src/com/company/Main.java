3511554ad608bc2661f52ed7d4b4c9f6b79f70fe
vovaanik <vovaanik@yandex.ru>
2020-09-09 14:34:55 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int savings = 2_000_000_000;
+        System.out.println(savings);
+        int transfer = 500_000_000;
+        System.out.println(transfer);
+        int balance = savings + transfer;
+        System.out.println(balance);
+    }
+}
